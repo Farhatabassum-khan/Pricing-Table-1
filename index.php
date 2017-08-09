c39109bde8775a1c16198eca2dde6e73b04555de
Farhatabassum Khan <khanfarhatabassum@gmail.com>
2017-08-09 22:44:33 +0530

    Index.php
    This File contains HTML code.

---
+++ index.php
@@ -0,0 +1,115 @@
+<!Doctype html>
+<html lang="en">
+	<head>
+		<title>Pricing Table 1</title>
+		<meta charset="UTF-8">
+		<!-- Bootstrap CDN-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+		<!-- Font awesome CDN-->
+		
+		<script src="https://use.fontawesome.com/8160b53ad6.js"></script>
+		
+		<!-- Custom CSS-->
+		<link rel="stylesheet" type="text/css" href="mystyles.css" media="screen" />
+	</head>
+	<body style="background: #5dbbb1;">
+		
+		<div class="container bd_background"><!-- Container -->
+			<h1 class="heading">Pricing Table 1</h1>
+					
+			<div class="row pricing_row">
+				
+				<!-- Free-->
+				<div class="col-sm-4 free_col col_background">
+					
+					<div class="icon">
+						<i class="fa fa-plane fa-3x free_icon"></i>
+					</div>
+					
+					<h1 class="title free_member">Free</h1>
+					
+					<div class="free_description"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+					
+					<div class="separator"></div>
+					
+					<div class="charges free">
+						<h1>Free</h1>
+						<p class="duration">Last Forever</p>
+					</div>
+					
+					<div class="row row_btn">
+						<div class="col-sm-2 col-xs-2 "></div>
+						<div class="col-sm-8 col-xs-8  btn_start">
+							<a href="#" class="start_btn"><p>START NOW</p></a>
+						</div>
+						<div class="col-sm-2 col-xs-2 "></div>
+					</div>
+				
+				</div>
+				
+				<!-- Premium-->
+				<div class="col-sm-4 premium_col col_background">
+				
+					<div class="icon">
+						<i class="fa fa-fighter-jet fa-3x premium_icon"></i>
+					</div>
+					
+					<h1 class="title premium_member">Premium</h1>
+					
+					<div class="free_description premium"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+					
+					<div class="separator"></div>
+					
+					<div class="charges premium">
+						<h1>$49</h1>
+						<p class="duration">Last Forever</p>
+					</div>
+					
+					<div class="row row_btn">
+						<div class="col-sm-2 col-xs-2"></div>
+						<div class="col-sm-8 col-xs-8 btn_start">
+							<a href="#" class="start_btn"><p>START NOW</p></a>
+						</div>
+						<div class="col-sm-2 col-xs-2"></div>
+					</div>
+				
+				</div>
+				
+				<!-- Plus-->
+				<div class="col-sm-4 plus_col col_background">
+				
+					<div class="icon">
+						<i class="fa fa-rocket fa-3x plus_icon"></i>
+					</div>
+					
+					<h1 class="title plus_member">Plus</h1>
+					
+					<div class="free_description"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+					
+					<div class="separator"></div>
+					
+					<div class="charges plus">
+						<h1>$79</h1>
+						<p class="duration">Last Forever</p>
+					</div>
+					
+					<div class="row row_btn">
+						<div class="col-sm-2 col-xs-2 "></div>
+						<div class="col-sm-8  col-xs-8 btn_start">
+							<a href="#" class="start_btn"><p>START NOW</p></a>
+						</div>
+						<div class="col-sm-2 col-xs-2"></div>
+					</div>
+				
+				</div>
+			</div>
+		
+		</div><!-- Container End -->
+	
+	
+	</body>
+</html>
